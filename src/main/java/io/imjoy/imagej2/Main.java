2ffa8199aa3a262db4791a5d6c0cd5cf7f1529e6
Curtis Rueden <ctrueden@wisc.edu>
2020-10-13 10:01:41 -0500

    Add initial version
    For now, need to run "mvn -Puberjar" to generate it.

---
+++ src/main/java/io/imjoy/imagej2/Main.java
@@ -0,0 +1,20 @@
+package io.imjoy.imagej2;
+
+import net.imagej.ImageJ;
+
+/**
+ * Launches ImageJ.
+ * 
+ * @author Curtis Rueden
+ */
+public final class Main {
+
+	private Main() {
+		// prevent instantiation of utility class
+	}
+
+	public static void main(final String... args) {
+		final ImageJ ij = new ImageJ();
+		ij.launch(args);
+	}
+}
