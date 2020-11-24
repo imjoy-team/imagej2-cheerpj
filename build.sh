30984b612dab9218a14090299ac838473c378e91
Wei Ouyang <oeway007@gmail.com>
2020-11-24 19:17:06 +0100

    remove pluginpath; small fix for the build script;

--- build.sh
+++ build.sh
@@ -7,9 +7,21 @@ test -f "$CHEERPJ_EXEC" || {
   exit 1
 }
 
+
+# Get patched imagej-1 from ImageJA.JS
+# [[ -d imagej-js-dist ]] || {
+#   curl -s https://api.github.com/repos/imjoy-team/ImageJA.JS/releases/latest | \
+#   grep "/imagej-js-dist.tgz" | \
+#   cut -d : -f 2,3 | \
+#   tr -d \" | \
+#   xargs -I {} curl -OL {}
+#   tar -xvzf imagej-js-dist.tgz
+#   rm imagej-js-dist.tgz
+# }
+# mvn install:install-file -Dfile=imagej-js-dist/ij153/ij-1.53f.jar -DgroupId=io.imjoy -DartifactId=imageja -Dversion=1.53f -Dpackaging=jar
+
 # Install cheerpj-dom.jar
 mvn install:install-file -Dfile=${CHEERPJ_HOME}/cheerpj-dom.jar -DgroupId=com.learningtech -DartifactId=cheerpj-dom -Dversion=1.0 -Dpackaging=jar
-
 mvn clean package
 
 jar=target/imagej2-cheerpj-0-SNAPSHOT-all.jar 
@@ -26,11 +38,11 @@ WORKDIR=./target/cheerpj_workdir
 }
 mkdir -p "$WORKDIR"
 "$CHEERPJ_EXEC" --work-dir="$WORKDIR" -j 4 "$jar"
-rm -rf "$WORKDIR"
+# rm -rf "$WORKDIR"
 
 # HACK: Restore ij.IJ class.
 mv "$backup" "$jar"
 
 cp -rp dist target/dist
 mv "$jar" target/dist
-mv "$jar".js target/dist
+mv "$jar"*.js target/dist
--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ test -d "$distdir" || {
   >&2 echo "Please run ./build.sh first."
   exit 1
 }
+cp -r "$dir"/dist/* "$distdir"
 cd "$distdir"
 python3 -m http.server 9999
 echo "Server running on http://localhost:9999"
